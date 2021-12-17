28ea2462f449268aea801cf08b8a6901b0d0b22b
OzkanOzsahin <92115875+OzkanOzsahin@users.noreply.github.com>
2021-12-17 18:15:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// This is a large sum
+console.log("Hello winc Academy students")
+
+/* This is block comment
+console.log(1230941 * 1823794);
+console.log(637263 / 54 );
+ */
+
+
+
+
+// Code die niet "aan" staat zie je niet in de console.
+
+// Does JavaScript run in your browser? Then you will find the console in your developer tools.
